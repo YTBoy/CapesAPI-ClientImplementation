e621fd5b72865ed70f4942f5209afe87796c07c3
Matthew Hatcher <MatthewSH@users.noreply.github.com>
2017-01-02 02:26:01 -0500

    Create CapesAPI.java

---
+++ CapesAPI.java
@@ -0,0 +1,14 @@
+package com.halfpetal.capesapi;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import net.minecraft.client.Minecraft;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+public class CapesAPI {
+}
